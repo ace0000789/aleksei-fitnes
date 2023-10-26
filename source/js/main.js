e14f874ce908de15f9bf0a3fae0167911c03b353
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-10-26 17:37:59 +0200

    add js for video

--- source/js/main.js
+++ source/js/main.js
@@ -46,3 +46,20 @@ window.addEventListener('DOMContentLoaded', () => {
 // breakpointChecker();
 
 // используйте .closest(el)
+
+
+// Видео
+let button = document.querySelector('.about-us__video-button');
+let container = document.querySelector('.about-us__video');
+
+button.addEventListener('click', function () {
+  if (button.classList.contains('ready')) {
+    return;
+  }
+
+  button.classList.add('ready');
+  container.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/9TZXsZItgdw?si=VTjj2qUjkgYfiWLC&amp;autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+  button.style.display = 'none';
+});
+
+/**/
