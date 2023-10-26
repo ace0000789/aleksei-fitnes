e450b3589721d26b47352b2348fe14c62f2fe730
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-10-26 18:43:56 +0200

    video work

--- source/js/main.js
+++ source/js/main.js
@@ -49,16 +49,19 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 // Видео
+
 let button = document.querySelector('.about-us__video-button');
 let container = document.querySelector('.about-us__video');
 
 button.addEventListener('click', function () {
+
   if (button.classList.contains('ready')) {
     return;
   }
 
   button.classList.add('ready');
-  container.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/9TZXsZItgdw?si=VTjj2qUjkgYfiWLC&amp;autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+  container.innerHTML = '<iframe src="https://www.youtube.com/embed/9TZXsZItgdw?si=VTjj2qUjkgYfiWLC&amp;autoplay=1" preload="auto" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+
   button.style.display = 'none';
 });
 
