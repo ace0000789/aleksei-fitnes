9ae82e2e94b31d66452f1eab88fee3a1d8da639b
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-10-31 17:19:51 +0100

    add data-validate for video js

--- source/js/vendor/video.js
+++ source/js/vendor/video.js
@@ -1,14 +1,14 @@
 export function initVideo() {
-  let buttonVideo = document.querySelector('.about-us__video-button');
-  let container = document.querySelector('.about-us__video');
+  const buttonVideo = document.querySelector('[data-validate="play-video"]');
+  let container = document.querySelector('[data-validate="container-video"]');
 
   buttonVideo.addEventListener('click', function () {
-
+    /*
     if (buttonVideo.classList.contains('ready')) {
       return;
     }
 
-    buttonVideo.classList.add('ready');
+    buttonVideo.classList.add('ready');*/
     container.innerHTML = '<iframe src="https://www.youtube.com/embed/9TZXsZItgdw?si=VTjj2qUjkgYfiWLC&amp;autoplay=1" preload="auto" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
 
     buttonVideo.style.display = 'none';
