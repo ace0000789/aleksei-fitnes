cdac0d692df3afdf1acafae51fe00a7fb3618522
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-11-14 13:45:27 +0100

    linter video function

--- source/js/init-video.js
+++ source/js/init-video.js
@@ -2,9 +2,9 @@ export function initVideo() {
   const buttonVideo = document.querySelector('[data-validate="play-video"]');
   const container = document.querySelector('[data-validate="container-video"]');
 
-  buttonVideo.addEventListener('click', function () {
+  function handleVideoButtonClick() {
     container.innerHTML = '<iframe src="https://www.youtube.com/embed/9TZXsZItgdw?si=VTjj2qUjkgYfiWLC&amp;autoplay=1" preload="auto" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
-
     buttonVideo.style.display = 'none';
-  });
+  }
+  buttonVideo.addEventListener('click', handleVideoButtonClick);
 }
