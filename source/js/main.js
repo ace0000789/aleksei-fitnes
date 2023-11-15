fbd143189f97f3b03071d8d5aba1cc6d2a4f6947
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-11-15 09:50:32 +0100

    delete coments js

--- source/js/main.js
+++ source/js/main.js
@@ -12,7 +12,7 @@ import {initSliderReview} from './init-slider-review';
 window.addEventListener('DOMContentLoaded', () => {
 
   // Modules
-  // ---------------------------------
+
   initVideo();
   initTabsSubscription();
   initSliderJury();
@@ -21,8 +21,6 @@ window.addEventListener('DOMContentLoaded', () => {
   initTabsFaq();
   initSliderReview();
 
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
 
     const form = new Form();
@@ -30,28 +28,3 @@ window.addEventListener('DOMContentLoaded', () => {
     form.init();
   });
 });
-
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используется matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
